5add0ddf7541a2c455146bd6dc6ca1203712de08
eloypassarella <eloypassa@gmail.com>
2021-05-20 22:23:56 -0300

    nuevo ecommerce maliba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Maliba voy a hacerte tu e-commerce. 
         </p>
         <a
           className="App-link"
